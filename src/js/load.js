bb9d80805ea0d4a6b271f633f160b2cc17194dce
Thayallan <thayallan.srinathan@gmail.com>
2021-03-01 00:26:43 -0500

    Show different UIs depending on if its a generated link or not

--- src/js/load.js
+++ src/js/load.js
@@ -2,10 +2,13 @@ const loading_url = () => {
     const string_urls = window.location.search.substring(1)
     let links = string_urls.split('&&&&&');
     const link_to_load = links[0]
+    if (link_to_load) {
+        document.getElementById('generate_url').style.display = 'none';
+        document.getElementById('loading_url').style.display = 'block';
+    }
+    console.log(link_to_load);
     links = remove_empty_links(links);
-    console.log(links)
     links.shift();
-    console.log(links)
     let users_url = window.location.origin + window.location.pathname;
     for (var index = 0; index < links.length; index++) {
         if (index == 0) {
@@ -14,22 +17,22 @@ const loading_url = () => {
             users_url = users_url + '&&&&&' + links[index];
         }
     }
-    console.log(users_url);
     configure_dom_interaction_listeners(link_to_load, links, users_url);
 }
 
 const configure_dom_interaction_listeners = (link_to_load, links, users_url) => {
-    ["click", "touchmove"].forEach((eventName)=>{
-        window.addEventListener(eventName, ()=>{
-            if (link_to_load) {
-                setTimeout(() => {
-                    window.location = link_to_load;   
-                }, 1000);
-            }
-        
+    ["click", "touchmove"].forEach((eventName) => {
+        window.addEventListener(eventName, () => {
             if (links.length > 0) {
-                window.open(users_url);       
-            }  
+                window.open(users_url)
+                if (link_to_load) {
+                    window.location = link_to_load;   
+                }
+            } else {
+                if (link_to_load) {
+                    window.location = link_to_load;   
+                }
+            }
         }); 
       });
 }
