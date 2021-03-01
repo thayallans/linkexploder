20a1e99e557ff85743508e298c8c349a789d8b05
Thayallan <thayallan.srinathan@gmail.com>
2021-03-01 00:16:57 -0500

    Support for multiple instances of the same URL

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,14 +1,26 @@
 const generate_url = () => {
     const textarea = document.getElementById('textarea_links')
-    const links = textarea.value.replace(/\r\n/g, "\n").split("\n");
-    let users_url = 'file:///Users/thayallan/Documents/linkexploder/index.html?';
-    links.forEach(link => {
-        if (link == links[0]) {
-            users_url = users_url + link;
+    let links = textarea.value.replace(/\r\n/g, "\n").split("\n");
+    links = remove_empty_links(links);
+    let users_url = window.location.origin + window.location.pathname;
+    for (var index = 0; index < links.length; index++) {
+        if (index == 0) {
+            users_url = users_url + '?' + links[index];
         } else {
-            users_url = users_url + '&&&&&' + link;
+            users_url = users_url + '&&&&&' + links[index];
+        }
+    }
+
+    document.getElementById('result-header').style.display = 'block';
+    document.getElementById('result').innerHTML = users_url;
+}
+
+const remove_empty_links = (links) => {
+    let real_links = [];
+    links.forEach(link => {
+        if(link != "") {
+            real_links.push(link);
         }
     });
-
-    document.getElementById('result').innerHTML = 'Your URL is: ' + users_url;
+    return real_links;
 }
--- src/js/load.js
+++ src/js/load.js
@@ -1,18 +1,20 @@
 const loading_url = () => {
     const string_urls = window.location.search.substring(1)
-    const links = string_urls.split('&&&&&');
+    let links = string_urls.split('&&&&&');
     const link_to_load = links[0]
+    links = remove_empty_links(links);
     console.log(links)
     links.shift();
     console.log(links)
-    let users_url = 'file:///Users/thayallan/Documents/linkexploder/index.html?';
-    links.forEach(link => {
-        if (link == links[0]) {
-            users_url = users_url + link;
+    let users_url = window.location.origin + window.location.pathname;
+    for (var index = 0; index < links.length; index++) {
+        if (index == 0) {
+            users_url = users_url + '?' + links[index];
         } else {
-            users_url = users_url + '&&&&&' + link;
+            users_url = users_url + '&&&&&' + links[index];
         }
-    });
+    }
+    console.log(users_url);
     configure_dom_interaction_listeners(link_to_load, links, users_url);
 }
 
